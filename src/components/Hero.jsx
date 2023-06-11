1923dcf31d22efbe161d7ebbaec9739762f7274f
hanbintan2003 <thb182003@gmail.com>
2023-06-10 22:41:49 -0700

    having css bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {logo} from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center
+     items-center flex-col'>
+        <nav className='flex justify-between items-center w-full
+        mb-10 pt-3'>
+            
+            <img src={logo} alt="sumz_logo" 
+            className= 'w-28 object-contain '/>
+            
+            <button type='button'
+            onClick={() => window.open('https://github.com/hanbintan2003/')}
+            className ='black_btn'>
+                Github
+            </button>
+        </nav>
+    </header>
   )
 }
 
