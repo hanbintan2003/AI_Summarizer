814ea8ddb26ebea93c785dc9d196a763644a5bdf
hanbintan2003 <thb182003@gmail.com>
2023-06-11 01:06:14 -0700

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    //need to in order to purge css from tailwind
+  ],
   theme: {
     extend: {
       fontFamily: {
