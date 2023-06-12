55e7c12fbd7e63f4e809c7f34be881ddcdd8a761
hanbintan2003 <thb182003@gmail.com>
2023-06-11 17:37:00 -0700

    semi Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,24 @@ import {logo} from '../assets';
 //unknow bug for css is not applied on mac
 const Hero = () => {
   return (
-    <header className=' flex justify-center
-     items-center flex-col'>
-        <nav className='flex justify-center items-center w-full
-        mb-10 pt-3'>
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             
             <img src={logo} alt="sumz_logo" 
-            onClick={() => window.open('https://github.com/hanbintan2003/')}
-            className= 'w-4 h-auto object-contain mx-auto'
+            className= 'w-16 object-contain '
             />
-        </nav>
 
-        <h1 className='head_text'>
+            <button
+            className='black_btn'
+            onClick={() => window.open('https://github.com/hanbintan2003/')}
+            >
+                Github
+            </button>
+        </nav>
+        
+        <h1 className='head_text '>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='blue_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ export default {
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'],
         inter: ['Inter', 'sans-serif'], 
+        keyframes: {
+          typing: {
+            '0%': { width: '0%' },
+            '100%': { width: '100%' }
+          },
+          blink: {
+            '0%, 100%': { borderColor: 'transparent' },
+            '50%': { borderColor: 'transparent' }
+          }
+        },
+        animation: {
+          typing: 'typing 5s steps(40, end), blink 0.75s step-end infinite'
+        }
       },
     },
   },
